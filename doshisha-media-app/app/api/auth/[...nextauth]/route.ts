b8099422111f372988c110b7632b97271d6cab99
mizu20040814 <mizu20040814@gmail.com>
2025-11-03 11:14:20 +0900

    secret修正

--- doshisha-media-app/app/api/auth/[...nextauth]/route.ts
+++ doshisha-media-app/app/api/auth/[...nextauth]/route.ts
@@ -48,7 +48,7 @@ export const authOptions: NextAuthOptions = {
         return session
         }
     },
-    secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET
+    secret: process.env.NEXTAUTH_SECRET
 }
 
 const handler = NextAuth(authOptions)
